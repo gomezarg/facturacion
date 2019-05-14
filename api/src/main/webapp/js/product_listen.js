75a625128aaabc95b7819f0cf5ed684873aa2638
vickysvenson <svensonvictoria1@gmail.com>
2019-05-13 21:16:03 -0300

    terminar product

---
+++ api/src/main/webapp/js/product_listen.js
@@ -0,0 +1,12 @@
+
+    var xhr = new XMLHttpRequest();
+    var url = "api/product";
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+        }
+    };
+ 
---
+++ api/src/main/webapp/js/purchase_creation.js
@@ -0,0 +1,18 @@
+function createPurchase() {
+    let purchase = {
+        product: parseInt(document.getElementById("product").value),
+        quantity: parseInt(document.getElementById("quantity").value)
+    };
+    var xhr = new XMLHttpRequest();
+    var url = "api/product";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+        }
+    };
+    xhr.send(JSON.stringify(purchase));
+    alert('Compra creada!');
+}
